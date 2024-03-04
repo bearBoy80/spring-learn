2a7ac926925e924055874ae79cb9d961a35c2f39
bearboy80 <2373319771@qq.com>
2024-03-04 18:38:24 +0800

    add placeholder demo

---
+++ spring-framework-learn/spring-ioc/src/main/java/com/github/bearboy/spring/enviroment/PropertyPlaceholderConfigurerDemo.java
@@ -0,0 +1,18 @@
+package com.github.bearboy.spring.enviroment;
+
+import com.github.bearboy.spring.ioc.overview.pojo.User;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PropertyPlaceholderConfigurerDemo {
+    public static void main(String[] args) {
+        // 创建并且启动 Spring 应用上下文
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/placeholders-resolver.xml");
+
+        User user = context.getBean("user", User.class);
+
+        System.out.println(user);
+
+        // 关闭 Spring 应用上下文
+        context.close();
+    }
+}
